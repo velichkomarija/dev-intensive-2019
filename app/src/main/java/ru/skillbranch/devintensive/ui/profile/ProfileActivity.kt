fa57bfeed296f32ed970dc0b5a4f56648efdc4c2
velichkomarija <velichkomarija4@gmail.com>
2020-04-14 13:49:36 +0300

    Дополнения ьазовой реализации.

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -9,8 +9,13 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile_constraint.*
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -18,6 +23,7 @@ class ProfileActivity : AppCompatActivity() {
         const val IS_EDIT_MODE = "IS_EDIT_MODE"
     }
 
+    private lateinit var viewModel: ProfileViewModel
     var isEditMode = false;
     lateinit var viewFields: Map<String, TextView>
 
@@ -25,6 +31,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_constraint)
         initViews(savedInstanceState)
+        initViewModel()
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
@@ -32,10 +39,28 @@ class ProfileActivity : AppCompatActivity() {
         outState.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileData().observe(this, Observer { updateUI(it) })
+        viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+    }
+
+    private fun updateTheme(mode: Int?) {
+        delegate.localNightMode = mode!!
+    }
+
+    private fun updateUI(profile: Profile) {
+        profile.toMap().also {
+            for ((k, v) in viewFields) {
+                v.text = it[k].toString()
+            }
+        }
+    }
+
     private fun initViews(savedInstanceState: Bundle?) {
 
         viewFields = mapOf(
-                "nickname" to tv_nick_name,
+                "nickName" to tv_nick_name,
                 "rank" to tv_rank,
                 "firstName" to et_first_name,
                 "lastName" to et_last_name,
@@ -48,9 +73,14 @@ class ProfileActivity : AppCompatActivity() {
         showCurrentMode(isEditMode)
 
         btn_edit.setOnClickListener {
+            if (it.isEnabled) saveProfileInfo()
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
         }
+
+        btn_switch_theme.setOnClickListener(View.OnClickListener {
+            viewModel.switchTheme()
+        })
     }
 
     private fun showCurrentMode(isEditMode: Boolean) {
@@ -71,7 +101,7 @@ class ProfileActivity : AppCompatActivity() {
         }
 
         ic_eye.visibility = if (isEditMode) View.GONE else View.VISIBLE
-        wr_about.isCounterEnabled = isEditMode
+        wr_about.isEnabled = isEditMode
 
         with(btn_edit) {
             val filter: ColorFilter? = if (isEditMode) {
@@ -83,13 +113,24 @@ class ProfileActivity : AppCompatActivity() {
             }
 
             val icon = if (isEditMode) {
-                resources.getDrawable(R.drawable.ic_save_black_24dp)
+                resources.getDrawable(R.drawable.ic_save_black_24dp, theme)
             } else {
-                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+                resources.getDrawable(R.drawable.ic_edit_black_24dp, theme)
             }
 
             background.colorFilter = filter
             setImageDrawable(icon)
+        }
+    }
+
+    private fun saveProfileInfo() {
+        Profile(
+                firstName = et_first_name.text.toString(),
+                lastName = et_last_name.text.toString(),
+                about = et_about.text.toString(),
+                repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileData(this)
         }
     }
 

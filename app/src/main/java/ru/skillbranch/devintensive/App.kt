68791deb9ae3f58989a485daf0bce6e71fdaf366
velichkomarija <velichkomarija4@gmail.com>
2020-04-14 13:52:14 +0300

    Сохранение темы.

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -2,22 +2,27 @@ package ru.skillbranch.devintensive
 
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
 
 class App : Application() {
 
     companion object {
         private var instance: App? = null
 
-        fun applicationContext() : Context{
+        fun applicationContext(): Context {
             return instance!!.applicationContext
         }
     }
 
-    init{
+    init {
         instance = this
     }
 
     override fun onCreate() {
         super.onCreate()
+        PreferencesRepository.getAppTheme().also {
+            AppCompatDelegate.setDefaultNightMode(it)
+        }
     }
 }
